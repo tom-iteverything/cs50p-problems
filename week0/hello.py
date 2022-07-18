6d2fcfe852a93d8f588f77b5bd3e060d9cd37e8c
Tom Kaufmann <tom@iteverything.io>
2022-07-17 20:46:45 -0400

    created funcitons to handle output

--- week0/hello.py
+++ week0/hello.py
@@ -1,5 +1,12 @@
-# Ask user for their name, 'strip' the whitespaces, capitalize it & split the name a part into two vars
-first, last = input("What is your name?: ").strip().title().split(" ")
+# Define the main() funciton or application
+def main():
+    # Ask user for their name, 'strip' the whitespaces, capitalize it & split the name a part into two vars
+    first, last = input("What is your name?: ").strip().title().split(" ")
+    # Print the full name
+    hello(first, last)
 
-# Print the full name
-print(f"Hello, {first} {last}.")
+# Define the help() function
+def hello(f, l):
+    print(f"Hello, {f} {l}.")
+
+main()
